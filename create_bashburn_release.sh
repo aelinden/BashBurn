724286f158b8485bc35d49e94b4555d7eb94aa59
Anders Lindén <anders.linden@gmail.com>
2011-07-17 03:02:52 +0200

    Small update in release creation script

--- create_bashburn_release.sh
+++ create_bashburn_release.sh
@@ -4,7 +4,6 @@ if [ $# -ne 1 ]; then
 	echo 'You must supply a version number as an argument.'
 else
 	cp -r trunk BashBurn-$1
-	tar cvf BashBurn-$1.tar BashBurn-$1
-	gzip BashBurn-$1.tar
+	tar cvfz BashBurn-$1.tar.gz BashBurn-$1
 	rm -rf BashBurn-$1
 fi
